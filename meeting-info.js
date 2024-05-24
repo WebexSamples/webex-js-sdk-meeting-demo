3eefcfb71e184e03c4263343b0c5ba4d3c8509db
Adam Weeks <adweeks@cisco.com>
2024-05-24 14:47:35 -0400

    refactor: style with prettier formatting

--- meeting-info.js
+++ meeting-info.js
@@ -1,14 +1,14 @@
 // This URL below can be replaced with any Virtual Background Image URL
-export const vbgImageUrl = './images/V_B_G.jpg';
+export const vbgImageUrl = "./images/V_B_G.jpg";
 
 // Obtain the following meetingInfo by creating a new meeting in the Webex Developer Portal
 // Read through this API Reference to know more:
 // https://developer.webex.com/docs/api/v1/meetings/create-a-meeting
 // Once meeting is created, copy the whole response and paste it here.
 // Note - Unless we have this information, this demo cannot join a meeting
-export const meetingInfo = {} // REQUIRED
+export const meetingInfo = {}; // REQUIRED
 
-// To get guestIssueAccessToken and guestEndpointUrl 
+// To get guestIssueAccessToken and guestEndpointUrl
 // Update the following with the Guest Issuer Access Token through Service Apps
 // Read more about it here: https://developer.webex.com/docs/sa-guest-management
 // Note - Unless we have this token, this demo cannot join a meeting
